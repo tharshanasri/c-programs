c4a2e7a5086b993b58c6535b5aa98e4f9f1efef8
tharshanasri <tharshanasri0@gmail.com>
2025-09-02 20:16:12 +0530

    Update tables code.c

--- tables code.c
+++ tables code.c
@@ -11,7 +11,7 @@ int main()
     printf("Multiplication table of %d:\n", num);
     while (i <= 10) 
          { 
-        printf("%d x %d = %d\n", num, i,            num * i);
+        printf("%d x %d = %d\n", num, i,        num * i);
         i++;
          }
 
