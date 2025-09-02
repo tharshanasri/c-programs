4e660ec1b016ad815fbecc6e9d43c5e652e56c6d
tharshanasri <tharshanasri0@gmail.com>
2025-09-02 20:15:12 +0530

    Create tables code.c

---
+++ tables code.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main() 
+{
+    int num, i;
+
+    // Input the number
+    printf("Enter a number: ");
+    scanf("%d", &num);
+    i=1;
+    // Print multiplication table
+    printf("Multiplication table of %d:\n", num);
+    while (i <= 10) 
+         { 
+        printf("%d x %d = %d\n", num, i,            num * i);
+        i++;
+         }
+
+    return 0;
+}
